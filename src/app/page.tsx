f68ef85cc6d06d93b0f9b2744c628b54d50159ad
westwood846 <PapstDonB@Googlemail.com>
2024-04-27 14:10:36 +0100

    Update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className="mb-12 text-center">Hanny&apos;s Cookbook</h1>
       <p className="mb-12 text-center">
         {
-          "'What do I have and what do I want?' I slowly pull out differentingredients, some of which I'm not super excited about, but need to be eaten soon. The kitchen is my stage and I'm an improviser, welcome to my show. Everything is real. I might clean up the counter a bit here and there, but generally, I show up on stage as it is. Pretty messy, confused, unscripted and most of the time hungry."
+          "'What do I have and what do I want?' I slowly pull out different ingredients, some of which I'm not super excited about, but need to be eaten soon. The kitchen is my stage and I'm an improviser, welcome to my show. Everything is real. I might clean up the counter a bit here and there, but generally, I show up on stage as it is. Pretty messy, confused, unscripted and most of the time hungry."
         }
       </p>
       <article className="flex flex-col gap-16">
