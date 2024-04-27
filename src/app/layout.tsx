431c31df62d9c1a9e7c081ac499cf87641f04c63
westwood846 <PapstDonB@Googlemail.com>
2024-04-27 14:11:34 +0100

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hanny's Cook Book",
+  title: "Hanny's Cookbook",
   description:
     "The kitchen is my stage and I'm an improviser, welcome to my show. Everything is real. I might clean up the counter a bit here and there, but generally, I show up on stage as it is. Pretty messy, confused, unscripted and most of the time hungry.",
 };
